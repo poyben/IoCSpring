40555beafc0bd392b2a8032afc46721cc0b8fdff
poyben <poybenglez@gmail.com>
2024-03-20 16:03:47 +0000

    Creado ServicioA

---
+++ src/main/java/com/paquete/ServicioA.java
@@ -0,0 +1,12 @@
+package com.paquete;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ServicioA {
+
+	public String getMensaje() {
+		return "Enviando mensajede ServicioA";
+	}
+	
+}
